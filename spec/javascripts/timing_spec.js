e0dc5314901e7c2d59c30a32660e9aebeeeabd2b
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-09-24 12:22:40 -0700

    Fix Linter complaints

--- spec/javascripts/timing_spec.js
+++ spec/javascripts/timing_spec.js
@@ -1,33 +1,33 @@
 function TimingRace() {}
 
-TimingRace.prototype.start = function() {  
+TimingRace.prototype.start = function() {
 
   if (this.start_time) {
     return false;
-  } 
+  }
   else {
     this.start_time = Date.now();
     return true;
   }
-}
+};
 
-TimingRace.prototype.isTimerOn = function() {  
+TimingRace.prototype.isTimerOn = function() {
   if (this.start_time && !this.stop_time) {
     return true;
-  } 
+  }
   else {
     return false;
   }
-}
+};
 
-TimingRace.prototype.stop = function() {  
+TimingRace.prototype.stop = function() {
   this.stop_time = Date.now();
-}
+};
 
-TimingRace.prototype.totalRaceTime = function() {  
-  this.total_race_time = this.stop_time - this.start_time
-  return this.total_race_time/1000.0
-}
+TimingRace.prototype.totalRaceTime = function() {
+  this.total_race_time = this.stop_time - this.start_time;
+  return this.total_race_time/1000.0;
+};
 
 
 
@@ -69,13 +69,4 @@ describe("Timing Race", function() {
       expect(timer.totalRaceTime()).toBeGreaterThan(0.199);
     });
   });
-
-
-
-
 });
-
-
-
-
-
