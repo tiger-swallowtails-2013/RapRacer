fe84d78f2ed4d0d6716642a4e2df65b683545034
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-09-24 16:48:23 -0700

    Add Players

---
+++ spec/javascripts/players_spec.js
@@ -0,0 +1,10 @@
+
+describe("Player", function(){
+  it("start position is zero", function(){
+    expect(player.position()).toEqual(0);
+  });
+  it("knows position on board", function(){
+
+  });
+});
+
