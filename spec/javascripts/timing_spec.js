8d025fde3d03afa174070a4726043ea196bd4967
dbcapprentice <dbcapprentice@gmail.com>
2013-09-25 16:19:11 -0700

    Move TimingRace Object to its own file and make sure its included in layour.erb

--- spec/javascripts/timing_spec.js
+++ spec/javascripts/timing_spec.js
@@ -1,36 +1,3 @@
-function TimingRace() {}
-
-TimingRace.prototype.start = function() {
-
-  if (this.start_time) {
-    return false;
-  }
-  else {
-    this.start_time = Date.now();
-    return true;
-  }
-};
-
-TimingRace.prototype.isTimerOn = function() {
-  if (this.start_time && !this.stop_time) {
-    return true;
-  }
-  else {
-    return false;
-  }
-};
-
-TimingRace.prototype.stop = function() {
-  this.stop_time = Date.now();
-};
-
-TimingRace.prototype.totalRaceTime = function() {
-  this.total_race_time = this.stop_time - this.start_time;
-  return this.total_race_time/1000.0;
-};
-
-
-
 describe("Timing Race", function() {
   var timer = new TimingRace();
   it("starts the timer", function() {
