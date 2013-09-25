7585618c679d971166964fa621f43060d6e6b082
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-09-24 17:26:02 -0700

    Add Players spec

--- public/javascripts/players.js
+++ public/javascripts/players.js
@@ -1,1 +1,7 @@
 
+function Player() {}
+
+Player.prototype.position = function(){
+  this.start_position = 0;
+  return this.start_position;
+};
--- spec/javascripts/players_spec.js
+++ spec/javascripts/players_spec.js
@@ -1,10 +1,15 @@
 
 describe("Player", function(){
+
   it("start position is zero", function(){
+    var player = new Player();
     expect(player.position()).toEqual(0);
   });
-  it("knows position on board", function(){
 
+  it("knows position on board", function(){
+    var player = new Player();
+    player.move(5);
+    expect(player.position).toEqual(5);
   });
 });
 
