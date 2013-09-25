1b4364a44c15997eb63b7a1db1f03c37dd673377
dbcapprentice <dbcapprentice@gmail.com>
2013-09-25 14:56:53 -0700

    Add ongoing code to access on a different machine, lol

--- public/javascripts/rapracer.js
+++ public/javascripts/rapracer.js
@@ -1,25 +1,42 @@
-var inputChecker = {
 
-  wordChecker: function(word1,word2) {
-    return word1 === word2
-  },
+var RapRacer = (function() {
+  var lyric_el = document.getElementById('lyric'),
+      lyric = new Lyric(lyric_el.innerText);
 
-  inputWordChecker: function(input_id,check_id) {
-    var inputWord = document.getElementById(input_id).value
-    var checkWord = document.getElementById(check_id).innerText
-    console.log(this.wordChecker(inputWord,checkWord))
-    return this.wordChecker(inputWord,checkWord)
-  },
 
-  userFeedback: function(input_id, check_id) {
-    var result = this.inputWordChecker(input_id, check_id)
-    if (result === true ) {
-      // alert('You did it!')
+  return {
+    lyric: function() {
+      return lyric.text();
     }
-    else {
-      // alert('You suck')
-    }
-    return result
   }
+})();
 
-};
+
+
+
+
+// var inputChecker = {
+
+//   wordChecker: function(word1,word2) {
+//     return word1 === word2
+//   },
+
+//   inputWordChecker: function(input_id,check_id) {
+//     var inputWord = document.getElementById(input_id).value
+//     var checkWord = document.getElementById(check_id).innerText
+//     console.log(this.wordChecker(inputWord,checkWord))
+//     return this.wordChecker(inputWord,checkWord)
+//   },
+
+//   userFeedback: function(input_id, check_id) {
+//     var result = this.inputWordChecker(input_id, check_id)
+//     if (result === true ) {
+//       // alert('You did it!')
+//     }
+//     else {
+//       // alert('You suck')
+//     }
+//     return result
+//   }
+
+// };
--- spec/javascripts/rapracer_spec.js
+++ spec/javascripts/rapracer_spec.js
@@ -1,5 +1,5 @@
 function highlight(text,index_value) {
-  
+
 }
 
 // describe("Highlighting Words", function() {
@@ -22,58 +22,68 @@ function highlight(text,index_value) {
 
 
 
-describe("wordChecker", function() {
-  it("should check words, return true if words match", function() {
-    expect(inputChecker.wordChecker("word","word")).toEqual(true);
-  });
+// describe("wordChecker", function() {
+//   it("should check words, return true if words match", function() {
+//     expect(inputChecker.wordChecker("word","word")).toEqual(true);
+//   });
 
-  it("should check words, return false if they don't match", function() {
-    expect(inputChecker.wordChecker("word","otherword")).toEqual(false);
+//   it("should check words, return false if they don't match", function() {
+//     expect(inputChecker.wordChecker("word","otherword")).toEqual(false);
+//   });
+// });
+
+// describe("inputWordChecker", function() {
+//   beforeEach(function() {
+//     createDomElement('textarea','user_bad_input')
+//     document.getElementById('user_bad_input').value = "raorao";
+
+//     createDomElement('textarea','user_good_input')
+//     document.getElementById('user_good_input').value = "Let";
+
+//     createDomElement('span','current_word')
+//     document.getElementById('current_word').innerText = "Let";
+//   });
+
+//   it ("should get values of two ids, return false if no match", function() {
+//     expect(inputChecker.inputWordChecker('user_bad_input','current_word')).toEqual(false);
+//   });
+
+
+//   it ("should get values of two ids, return true if match", function() {
+//     expect(inputChecker.inputWordChecker('user_good_input','current_word')).toEqual(true);
+//   });
+// });
+
+// describe("userFeedback", function() {
+//     beforeEach(function() {
+//     createDomElement('textarea','user_bad_input')
+//     document.getElementById('user_bad_input').value = "raorao";
+
+//     createDomElement('textarea','user_good_input')
+//     document.getElementById('user_good_input').value = "Let";
+
+//     createDomElement('span','current_word')
+//     document.getElementById('current_word').innerText = "Let";
+//   });
+
+//   it("should let the user know if the WordChecker returned true", function() {
+//     expect(inputChecker.userFeedback('user_good_input', 'current_word')).toEqual(true);
+//   })
+
+//   it("should let the user know if the WordChecker returned false", function() {
+//     expect(inputChecker.userFeedback('user_bad_input', 'current_word')).toEqual(false);
+//   })
+// })
+
+describe("RapRacer", function() {
+  it("has a lyric object that matches lyric value raorao'd in DOM", function() {
+    var dom_lyric = "I'm not a business man, I'm a business, man";
+    createDomElement('div','lyric');
+    document.getElementById('lyric').innerHTML = dom_lyric;
+
+    expect(RapRacer.lyric()).toEqual(dom_lyric);
   });
 });
-
-describe("inputWordChecker", function() {
-  beforeEach(function() {
-    createDomElement('textarea','user_bad_input')
-    document.getElementById('user_bad_input').value = "raorao";
-    
-    createDomElement('textarea','user_good_input')
-    document.getElementById('user_good_input').value = "Let";
-
-    createDomElement('span','current_word')
-    document.getElementById('current_word').innerText = "Let";
-  });
-
-  it ("should get values of two ids, return false if no match", function() {
-    expect(inputChecker.inputWordChecker('user_bad_input','current_word')).toEqual(false);
-  });
- 
-
-  it ("should get values of two ids, return true if match", function() {
-    expect(inputChecker.inputWordChecker('user_good_input','current_word')).toEqual(true);
-  });
-});
-
-describe("userFeedback", function() {
-    beforeEach(function() {
-    createDomElement('textarea','user_bad_input')
-    document.getElementById('user_bad_input').value = "raorao";
-    
-    createDomElement('textarea','user_good_input')
-    document.getElementById('user_good_input').value = "Let";
-
-    createDomElement('span','current_word')
-    document.getElementById('current_word').innerText = "Let";
-  });
-
-  it("should let the user know if the WordChecker returned true", function() { 
-    expect(inputChecker.userFeedback('user_good_input', 'current_word')).toEqual(true);
-  })
-
-  it("should let the user know if the WordChecker returned false", function() { 
-    expect(inputChecker.userFeedback('user_bad_input', 'current_word')).toEqual(false);
-  })
-})
 
 
 
