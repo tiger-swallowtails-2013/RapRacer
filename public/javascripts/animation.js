e490e81068a94c92e060d323a8cc713268211e70
Apprentice <apprentice@dbc16.local>
2013-09-27 17:52:00 -0700

    animation no longer calls dom on every movecharacter call

--- public/javascripts/animation.js
+++ public/javascripts/animation.js
@@ -8,8 +8,8 @@ var character = {
 
   left_pos: 0,
 
-  moveCharacter: function(){
-    this.avatar = document.getElementById('character_image');
+  moveCharacter: function() {
+    this.avatar = this.avatar || document.getElementById('character_image');
     this.left_pos = (this.left_pos + character.calcCharacterMovement());
     var percent_quantity = this.left_pos.toString() + "%";
     this.avatar.style.left = percent_quantity;
